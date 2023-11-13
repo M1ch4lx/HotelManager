e646bdf8ec9e0705ff214819501284c1196a683d
M1ch4lx <m.kowalczyk360@gmail.com>
2023-11-13 14:20:37 +0100

    Rest

--- HotelManagerFrontend/src/test/java/pl/edu/agh/hotelmanagerfrontend/command/DisplayRoomDetailsTest.java
+++ HotelManagerFrontend/src/test/java/pl/edu/agh/hotelmanagerfrontend/command/DisplayRoomDetailsTest.java
@@ -10,14 +10,6 @@ class DisplayRoomDetailsTest {
 
     @Test
     void test() {
-        var hotel = new Hotel();
-        hotel.addRoom(new Room(101, "Nice room", 300.0));
-        hotel.addRoom(new Room(102, "Very nice room", 250.0));
-        hotel.addRoom(new Room(201, "Mediocre room", 150.0));
-        hotel.addRoom(new Room(202, "Terrible room", 100.0));
 
-        assertDoesNotThrow(() -> {
-            new DisplayRoomDetails().execute(hotel);
-        });
     }
 }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
-Program
+Niestety coś zamieszałem się w konfiguracji i projektu nie można zbudować,
+można go jedynie uruchomić za pomocą IntelliJ
